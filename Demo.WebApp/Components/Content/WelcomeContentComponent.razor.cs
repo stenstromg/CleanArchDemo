dc2a685ff2cb121fc1fdcc6b27f397729185c196
stenstromg <gary.stenstrom@outlook.com>
2024-11-24 18:09:01 -0500

    Layout applied

---
+++ Demo.WebApp/Components/Content/WelcomeContentComponent.razor.cs
@@ -0,0 +1,28 @@
+﻿using Demo.WebApp.Classes;
+
+namespace Demo.WebApp.Components.Content
+{
+    public partial class WelcomeContentComponent : UnqComponentBase
+    {
+        #region properties
+        #endregion properties
+
+        #region parameters
+        #endregion parameters
+
+        #region data
+        #endregion data
+
+        #region lifecycle
+        #endregion lifecycle
+
+        #region event handlers
+        #endregion event handlers
+
+        #region private
+        #endregion private
+
+        #region public
+        #endregion public
+    }
+}
---
+++ Demo.WebApp/Components/Layout/MainLayout.razor.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Components;
+
+namespace Demo.WebApp.Components.Layout
+{
+    public partial class MainLayout : LayoutComponentBase
+    {
+        #region properties
+
+        bool sidebar1Expanded { get; set; } = true;
+
+        #endregion properties
+
+        #region data
+        #endregion data
+
+        #region ctor
+        #endregion ctor
+
+        #region event handlers
+
+
+        void Toggle_OnClick()
+        {
+            sidebar1Expanded = !sidebar1Expanded;
+            StateHasChanged();
+        }
+
+        #endregion event handlers
+
+        #region private
+        #endregion private
+
+        #region public
+        #endregion public
+    }
+}
